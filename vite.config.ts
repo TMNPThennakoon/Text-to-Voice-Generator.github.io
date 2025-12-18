9a36091dc576b7a85c0fd0ee58f67905bed2ade1
NAPI <napi@example.com>
2025-12-18 12:50:57 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Use the GitHub repository name as the base path for GitHub Pages
-  base: '/Text-to-Voice-Generator.github.io/',
+  base: '/',
 })
 
